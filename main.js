676df99783439d912f8b178fe0c339d53d05bd8b
김용민 <dydals3440@gmail.com>
2023-02-11 16:31:29 +0900

    JS: navbar-menu click

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  //   undefined 처리 (navbar 메뉴가 아닌 곳을 클릭시 undefined)
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link === null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
